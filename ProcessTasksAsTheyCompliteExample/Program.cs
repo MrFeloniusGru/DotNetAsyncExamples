17ee97dc40beade076c7bf20229544fdb1b474b6
StepanSazykin <stepan.sazykin@rt.ru>
2022-09-19 11:33:56 +0500

    ProcessTasksAsTheyCompliteExample init commut

---
+++ ProcessTasksAsTheyCompliteExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using ProcessTasksAsTheyCompliteLib;
+
+internal class Program
+{
+    private async static Task Main(string[] args)
+    {
+        async Task<int> WaitAsync(int s)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(s));
+            return s;
+        }
+
+
+        Task<int>[] tasks = new[]
+        {
+            WaitAsync(3),
+            WaitAsync(1),
+            WaitAsync(2)
+        };
+
+        foreach (var bucket in tasks.Interleaved())
+        {
+            var task = await bucket;
+            Console.WriteLine(await task);
+        }
+    }
+}
---
+++ ProcessTasksAsTheyCompliteLib/TaskExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProcessTasksAsTheyCompliteLib
+{
+    public static class TaskExtension
+    {
+        public static Task<Task<T>>[] Interleaved<T>(this IEnumerable<Task<T>> tasks)
+        {
+            var inputTasks = tasks.ToList();
+
+            var buckets = new TaskCompletionSource<Task<T>>[inputTasks.Count];
+            var results = new Task<Task<T>>[buckets.Length];
+            for (int i = 0; i < buckets.Length; i++)
+            {
+                buckets[i] = new TaskCompletionSource<Task<T>>();
+                results[i] = buckets[i].Task;
+            }
+
+            int nextTaskIndex = -1;
+            Action<Task<T>> continuation = completed =>
+            {
+                var bucket = buckets[Interlocked.Increment(ref nextTaskIndex)];
+                bucket.TrySetResult(completed);
+            };
+
+            foreach (var inputTask in inputTasks)
+                inputTask.ContinueWith(continuation, CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+
+            return results;
+        }
+    }
+}
